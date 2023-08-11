ca445d97ab7caa0d322b83af4c97ec5e6d47d048
AJGrey <greyajayi@yahoo.com>
2023-08-11 23:07:57 +0000

    error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort
+from flask import jsonify, abort, Blueprint
 from api.v1.views import app_views
 
 
@@ -24,3 +24,13 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+app_views = Blueprint('app_views', __name__)
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
+def raise_unauthorized_error() -> str:
+    """ GET /api/v1/unauthorized
+    """
+    abort(401)
+
